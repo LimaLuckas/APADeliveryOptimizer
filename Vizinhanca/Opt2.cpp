00c3f893cc21b101fcadd0ecb11732868095c44f
Caio Vinicius <caio81887@gmail.com>
2023-10-24 23:23:36 -0300

    corrigindo metodo

--- Vizinhanca/Opt2.cpp
+++ Vizinhanca/Opt2.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include <vector>
 
-// Função para realizar a operação 2-opt em uma solução
+// Função para realizar a operação 2-opt em uma solução inicial
 std::vector<int> Opt2(const std::vector<int>& solucao, int i, int j) {
     std::vector<int> novaSolucao = solucao;
     int aux = 0;
 
-    // Inverte os elementos entre i e j
-    while (i < j) {
-        aux = novaSolucao[j];
-        novaSolucao[j] = novaSolucao[i];
-        novaSolucao[i] = aux;
-        i++;
-        j--;
-    }
 
+    // Inverte os elementos entre i e j
+    for (i ; i < j; i++)
+    {
+        for (j ; j > i; j--)
+        {            
+            aux = novaSolucao[j];
+            novaSolucao[j] = novaSolucao[i];
+            novaSolucao[i] = aux;
+        }
+    }
+    
     return novaSolucao;
 }
