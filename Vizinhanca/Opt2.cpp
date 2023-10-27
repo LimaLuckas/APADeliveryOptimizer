83bd295a471e97b5e1e50599395c6dfa8b2d4550
Caio Vinicius <caio81887@gmail.com>
2023-10-27 19:46:11 -0300

    Ajuste de calculo

--- Vizinhanca/Opt2.cpp
+++ Vizinhanca/Opt2.cpp
@@ -2,21 +2,53 @@
 #include <vector>
 
 // Função para realizar a operação 2-opt em uma solução inicial
-std::vector<int> Opt2(const std::vector<int>& solucao, int i, int j) {
+std::vector<int> Opt2(const std::vector<int>& solucao, const std::vector<std::vector<int>>& custos, int custoAtual) {
     std::vector<int> novaSolucao = solucao;
     int aux = 0;
+    int novoCusto = 0;
+    int best_Value = custoAtual;
+    int best_i = 0;
+    int best_j = 0;
 
+    //Cálculo do melhor custo e intervalo para a nova solução
+    for (int i = 1 ; i < solucao.size()-3; i++){  // i só percorre até o (tamanho do vetor) - 3, para evitar movimentos de Swap
+        for (int j = i + 3; j < solucao.size(); j++){  // j começa em i+3 para evitar que vire um movimento de Swap
 
-    // Inverte os elementos entre i e j
-    for (i ; i < j; i++)
-    {
-        for (j ; j > i; j--)
-        {            
-            aux = novaSolucao[j];
-            novaSolucao[j] = novaSolucao[i];
-            novaSolucao[i] = aux;
+            novoCusto = custoAtual;
+            
+            //Subtração dos arcos originais que serão retirados
+            for (int k = i; k <= j + 1; k++)
+            {
+                novoCusto = novoCusto - custos[novaSolucao[k-1]][novaSolucao[k]];
+            }
+
+            //Soma dos extremos i e j já invertidos
+            novoCusto = novoCusto + custos[novaSolucao[i]][novaSolucao[j+1]] + custos[novaSolucao[i-1]][novaSolucao[j]];
+
+            //Soma dos arcos invertidos
+            for (int k = j; k > i; k--)
+            {
+                novoCusto = novoCusto + custos[novaSolucao[k]][novaSolucao[k-1]];
+            }
+            
+            if(novoCusto < custoAtual){
+              best_Value = novoCusto;
+              best_i = i;
+              best_j = j;
+            }          
         }
     }
+
+    // Inverte os elementos entre i e j
+    while (best_i < best_j)
+    {
+        aux = novaSolucao[best_j];
+        novaSolucao[best_j] = novaSolucao[best_i];
+        novaSolucao[best_i] = aux;
+        best_i++;
+        best_j--;
+    }
+    
     
     return novaSolucao;
 }
